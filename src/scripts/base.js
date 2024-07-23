ad516c20b65d8dde6589da2410fd3a17a26ffe2f
FoxAzure <108664801+FoxAzure@users.noreply.github.com>
2024-07-23 16:24:54 -0300

    Add files via upload

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -1,3 +1,10 @@
 const Relatorios = [
-    {id:0, tag: "perdas", url: ""}
+    {id:0, tag: "perdas", relatorio: "Perdas na Colheita Mecanizada", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiNTZkNzc5MGYtNThjOS00NTVmLTllNTAtZTA4ZTg4ZjMyNTgwIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:1, tag: "perdas", relatorio: "Perdas na Colheita Manual", ativo: true, url: ""},
+    {id:2, tag: "perdas", relatorio: "Relatório por Campo", ativo: false, url: ""},
+    {id:2, tag: "perdas", relatorio: "Relatório Estimado x Real", ativo: false, url: ""},
+    {id:3, tag: "perdas", relatorio: "Relatório das Colhedoras", ativo: true, url: ""},
+    {id:4, tag: "perdas", relatorio: "Relatório por Frente", ativo: true, url: ""},
+    {id:5, tag: "perdas", relatorio: "Relatório por Variedade", ativo: true, url: ""},
 ]
+
---
+++ src/scripts/index.js
@@ -0,0 +1,67 @@
+const User = [
+    {id: 0, user: "ADMIN", senha: "@target556", name: "Administrador", level: 0},
+    {id: 1, user: "PAULO", senha: "target2024", name: "Paulo Roberto", level: 0},
+    {id: 2, user: "ROMARIO", senha: "target2024", name: "Romario Lima", level: 0},
+    {id: 3, user: "FELIPE", senha: "target2024", name: "Felipe Vitturi", level: 0},
+    {id: 4, user: "PLANTIO", senha: "agrovale2024", name: "Plantio", level: 1},
+]
+
+
+let txt_user;
+let txt_senha;
+let txt_userUP;
+
+function validarlogin(){
+    txt_user = document.querySelector("#user").value
+    txt_senha = document.querySelector("#senha").value
+    
+    txt_userUP = txt_user.toUpperCase()
+
+    let resultado = User.filter( item =>
+        item.user == txt_userUP && item.senha == txt_senha
+    )
+    if(resultado.length>0){
+
+        let userName = resultado[0].user
+        let nickName = resultado[0].name
+        let levelUser = resultado[0].level
+
+
+        localStorage.setItem("target_User",userName)
+        localStorage.setItem("target_Name",nickName)
+        localStorage.setItem("target_Level",levelUser)
+        
+        window.location.assign("../inicio.html")
+        
+    }else{
+        alert("Login ou senha inválidos. Verifique e tente novamente.")
+    }
+}
+
+
+
+
+
+function checklogin(){
+    let username = document.querySelector("#username")
+    let user = localStorage.getItem("target_User");
+    let usernick = localStorage.getItem("target_Name");
+    let level = localStorage.getItem("target_Level");
+
+    let resultado = User.filter( item =>
+        item.user == user && item.level == level
+    )
+    
+    if(resultado.length==0){
+        window.location.assign("../index.html")
+    }else{
+        username.innerHTML = "Bem vindo, " + usernick
+    }
+}
+
+function quitlogin(){
+    localStorage.removeItem("target_User")
+    localStorage.removeItem("target_Name")
+    localStorage.removeItem("target_Level")
+    window.location.assign("../index.html")
+}
