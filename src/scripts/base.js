f10bf7dbb98563692b42f0b2f264b5117ca602b8
FoxAzure <108664801+FoxAzure@users.noreply.github.com>
2024-08-02 21:37:51 -0300

    Add files via upload

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -1,49 +1,52 @@
 const Relatorios = [
     //Equipe da Qualidade
-    {id:0, tag: 0, relatorio: "Acompanhamento da Equipe", ativo: true, url: ""},
-    {id:1, tag: 0, relatorio: "Semanal da Qualidade", ativo: true, url: ""},
-    {id:2, tag: 0, relatorio: "Mensal da Qualidade", ativo: true, url: ""},
-    {id:3, tag: 0, relatorio: "Mapa das Avaliações", ativo: true, url: ""},
+    {id:100, tag: 0, relatorio: "Acompanhamento da Equipe", ativo: true, url: ""},
+    {id:101, tag: 0, relatorio: "Semanal da Qualidade", ativo: true, url: ""},
+    {id:102, tag: 0, relatorio: "Mensal da Qualidade", ativo: true, url: ""},
+    {id:103, tag: 0, relatorio: "Mapa das Avaliações", ativo: true, url: ""},
 
     //Perdas na Colheita
-    {id:4, tag: 1, relatorio: "Perdas na Colheita Mecanizada", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiODE4MDU0YzItYjBhYS00ZTRmLTk3MWEtMGM0YTAyZTBkZjA4IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
-    {id:5, tag: 1, relatorio: "Perdas na Colheita Manual", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiYTU3YWJkMzItNDM4NC00M2Y4LTlkOTctM2EzZTRhYWI5NTQ4IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
-    {id:6, tag: 1, relatorio: "Relatório por Campo", ativo: false, url: ""},
-    {id:7, tag: 1, relatorio: "Relatório Estimado x Real", ativo: false, url: ""},
-    {id:8, tag: 1, relatorio: "Relatório das Colhedoras", ativo: true, url: ""},
-    {id:9, tag: 1, relatorio: "Relatório por Frente", ativo: true, url: ""},
-    {id:10, tag: 1, relatorio: "Relatório por Variedade", ativo: true, url: ""},
+    {id:300, tag: 2, relatorio: "Perdas na Colheita Mecanizada", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiODE4MDU0YzItYjBhYS00ZTRmLTk3MWEtMGM0YTAyZTBkZjA4IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:301, tag: 2, relatorio: "Perdas na Colheita Manual", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiYTU3YWJkMzItNDM4NC00M2Y4LTlkOTctM2EzZTRhYWI5NTQ4IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:302, tag: 2, relatorio: "Relatório por Campo", ativo: false, url: ""},
+    {id:303, tag: 2, relatorio: "Relatório Estimado x Real", ativo: false, url: ""},
+    {id:304, tag: 2, relatorio: "Relatório das Colhedoras", ativo: true, url: ""},
+    {id:305, tag: 2, relatorio: "Relatório por Frente", ativo: true, url: ""},
+    {id:306, tag: 2, relatorio: "Relatório por Variedade", ativo: true, url: ""},
 
     //Perparo de Solo
-    {id:11, tag: 2, relatorio: "Preparo de Solo", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiNjJmNDI0NmEtNTNiNy00M2E0LWIzYmQtODIwMWI1NmMwMTIzIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
-    {id:12, tag: 2, relatorio: "Profund. Cana x Fita Gotejadora", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiZTQ1NGJkZjktMjg2OC00M2QwLTk3YTItOTUwYjg3YjU0NGM3IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:400, tag: 3, relatorio: "Preparo de Solo", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiNjJmNDI0NmEtNTNiNy00M2E0LWIzYmQtODIwMWI1NmMwMTIzIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:401, tag: 3, relatorio: "Profund. Sulco x Fita Gotejadora", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiZTQ1NGJkZjktMjg2OC00M2QwLTk3YTItOTUwYjg3YjU0NGM3IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:402, tag: 3, relatorio: "Histórico Profundidade do Preparo", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiZTY1MTVkYjktMjUwNS00ZTk5LWJiZDMtY2FlOGYyOTRkMTY2IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:403, tag: 3, relatorio: "Histórico Paralelismo", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiYWU2MzY1OTMtMWMxNy00N2YwLWEwZmYtYjQ4YWQwNWQ5MmJlIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
 
     //Semente Mecanizada
-    {id:13, tag: 3, relatorio: "Semente Mecanizada", ativo: true, url: ""},
+    {id:500, tag: 4, relatorio: "Semente Mecanizada", ativo: true, url: ""},
 
     //Plantio Mecanizado
-    {id:14, tag: 4, relatorio: "Plantio Mecanizada", ativo: true, url: ""},
-
-    //IUP
-    {id:15, tag: 5, relatorio: "IUP - Última Avaliação", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiNDEyZmI5YzQtYThjZS00ZWJlLWFmNjMtOWQ1OGE0MGRlYzJiIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
-    {id:16, tag: 5, relatorio: "IUP - Campos Avaliados", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiYzhkYTFmYTgtZWIxYS00OTAyLWFjZjMtYWRlMDViYzlkYjE2IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:600, tag: 5, relatorio: "Plantio Mecanizada", ativo: true, url: ""},
 
     //Drone
-    {id:17, tag: 6, relatorio: "Drone - Última Avaliação", ativo: true, url: ""},
-    {id:18, tag: 6, relatorio: "Avaliação Adubação Socaria", ativo: true, url: ""},
-    {id:19, tag: 6, relatorio: "Avaliação Vinhaça Localizada", ativo: true, url: ""},
+    {id:700, tag: 6, relatorio: "Drone - Última Avaliação", ativo: true, url: ""},
+    {id:701, tag: 6, relatorio: "Avaliação Adubação Socaria", ativo: true, url: ""},
+    {id:702, tag: 6, relatorio: "Avaliação Vinhaça Localizada", ativo: true, url: ""},
+
+    //IUP
+    {id:800, tag: 7, relatorio: "IUP - Última Avaliação", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiNDEyZmI5YzQtYThjZS00ZWJlLWFmNjMtOWQ1OGE0MGRlYzJiIiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    {id:801, tag: 7, relatorio: "IUP - Campos Avaliados", ativo: true, url: "https://app.powerbi.com/view?r=eyJrIjoiYzhkYTFmYTgtZWIxYS00OTAyLWFjZjMtYWRlMDViYzlkYjE2IiwidCI6IjdkYjQwZjM0LTk1MzYtNGJiNC1iODVhLThhZDViMjEwODU0OCJ9"},
+    
 ]
 
 
 const Grupo = [
     {id: 0, tag: 0, nome: "Equipe Qualidade", level:"0", ativo: true},
-    {id: 7, tag: 7, nome: "Boletim Diário Qualidade", level:"1", ativo: false},
-    {id: 1, tag: 1, nome: "Perdas na Colheita", level: "1", ativo: true},
-    {id: 2, tag: 2, nome: "Preparo de Solo", level: "1", ativo: true},
-    {id: 3, tag: 3, nome: "Semente Mecanizada", level: "1", ativo: true},
-    {id: 4, tag: 4, nome: "Plantio Mecanizado", level: "1", ativo: true},
+    {id: 1, tag: 1, nome: "Boletim Diário Qualidade", level:"1", ativo: false},
+    {id: 2, tag: 2, nome: "Perdas na Colheita", level: "1", ativo: true},
+    {id: 3, tag: 3, nome: "Preparo de Solo", level: "1", ativo: true},
+    {id: 4, tag: 4, nome: "Semente Mecanizada", level: "1", ativo: true},
+    {id: 5, tag: 5, nome: "Plantio Mecanizado", level: "1", ativo: true},
     {id: 6, tag: 6, nome: "Tratos Culturais", level: "1", ativo: true},
-    {id: 5, tag: 5, nome: "IUP - Índice de Uniformidade do Plantio", level: "1", ativo: true},
+    {id: 7, tag: 7, nome: "IUP - Índice de Uniformidade do Plantio", level: "1", ativo: true},
 ]
 
 
